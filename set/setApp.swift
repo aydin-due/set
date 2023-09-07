b5a52ca202ff235b3636b7e4baf16456898f1ebc
aydin :^) <a219201085@unison.mx>
2023-09-07 11:08:20 -0700

    Initial Commit

---
+++ set/setApp.swift
@@ -0,0 +1,17 @@
+//
+//  setApp.swift
+//  set
+//
+//  Created by aydin salman on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct setApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
